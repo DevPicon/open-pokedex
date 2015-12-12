ea2f8c719a247e978975364f072a39a7a8492479
Armando <apiconz@gmail.com>
2015-12-12 08:26:41 -0500

    DetailLayout almost finished

---
+++ app/src/main/java/pe/picondev/android/openpokedex/presentation/activities/PokemonDetailActivity.java
@@ -0,0 +1,17 @@
+package pe.picondev.android.openpokedex.presentation.activities;
+
+import pe.picondev.android.openpokedex.R;
+
+public class PokemonDetailActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate() {
+
+    }
+
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_pokemon_detail;
+    }
+}
